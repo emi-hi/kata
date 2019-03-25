e3bf57ca1e3b047d11959c3abb3f18d8cd25e36d
emi-hi <emily.hillier@gmail.com>
2019-03-25 02:33:24 +0000

    added functionality for case list

--- casemaker2.js
+++ casemaker2.js
@@ -1,89 +1,135 @@
-
 const makeCase = function(input, casetype) {
   const space_replace = function(phrase, char) {
-    var message = '';
+    var message = "";
     for (var a = 0; a < phrase.length; a++){
-      if (phrase[a] == ' '){
+      if (phrase[a] == " "){
         message += char
       }
-      else if (phrase[a] != ' '){
+      else if (phrase[a] != " "){
         message += phrase[a];
       }
     }
     return message
   }
-  var outputMessage = '';
-  if (casetype == 'pascal' || casetype == 'title'){
-    var capitalizeFlag = 'true';
-  }
-  else {
-    var capitalizeFlag = 'false';
-  }
-  //for each letter in input string
-  for (var i = 0; i < input.length; i++){
-    if (casetype == 'camel' || casetype == 'pascal'|| casetype == 'title'){
-    //CAMEL - remove spaces and make first character after space a capitalize
-    //PASCAL - capitalize first char, remove spaces and capitalize first char after space
-    //TITLE - first char after space UpperCase
-      if (input[i] == ' '){
-         capitalizeFlag = 'true';
-         outputMessage += input[i];
+  const firstcase = function(input, casetype){
+    if (casetype == "camel" || casetype == "pascal" ||casetype == "title"){
+      if (input[i] == " "){
+        capitalizeFlag = "true";
+        firstcase_message += input[i];
       }
-      else if (input[i] != ' '){
-        if (capitalizeFlag == 'false'){
-        outputMessage += input[i];
+      else if (input[i] != " "){
+        if (capitalizeFlag == "false"){
+          firstcase_message += input[i];
+          }
+        else {
+          firstcase_message += input[i].toUpperCase();
+          capitalizeFlag = "false";
+        }
+      }
+    }
+    if (casetype == "camel"){
+      firstcase_message = space_replace(firstcase_message, "");
+      //CAMEL - remove spaces and make first character after space a capitalize
+    }
+    if (casetype == "pascal"){
+    //PASCAL - capitalize first char, remove spaces and capitalize after space
+      firstcase_message = space_replace(firstcase_message, "");
+    }
+    if (casetype == "snake"){  //SNAKE - convert spaces to underscore
+      firstcase_message = space_replace(input, "_");
+    }
+    if (casetype == "kebab"){  //KEBAB - convert spaces to dash
+      firstcase_message = space_replace(input, "-");
+    }
+    return firstcase_message;
+  }
+  const secondcase = function(input, casetype){
+    if (casetype == "vowel"){
+      //VOWEL - every vowel UpperCase
+        if (input[i] == "a" ||input[i] == "e" ||input[i] == "i"
+        ||input[i] == "o" ||input[i] == "u"){
+          secondcase_message += input[i].toUpperCase();
         }
         else {
-          outputMessage += input[i].toUpperCase();
-        capitalizeFlag = 'false';
+          secondcase_message += input[i];
+        }
+      }
+    if (casetype == "consonant"){
+      //CONSONANT - every consonant UpperCase
+      if (input[i] != "a" && input[i] != "e" && input[i] != "i"
+      && input[i] != "o" && input[i] != "u"){
+        secondcase_message += input[i].toUpperCase();
+      }
+      else {
+        secondcase_message += input[i];
+      }
+    }
+    return secondcase_message
+    }
+  const thirdcase = function(input, casetype){
+    if (casetype == "upper"){  //UPPER - convert to Upper
+      thirdcase_message += input[i].toUpperCase();
+    }
+    if (casetype == "lower"){  //LOWER - convert to Lower
+      thirdcase_message += input[u].toLowerCase();
+    }
+  return thirdcase_message;
+  }
+  var outputMessage = "";
+  var firstcase_message = "";
+  var secondcase_message = "";
+  var thirdcase_message = "";
+  if (casetype == "pascal" || casetype == "title"){
+    var capitalizeFlag = "true";
+  }
+  else {
+    var capitalizeFlag = "false";
+  }
+  var caselist = [];
+  var isarray = Array.isArray(casetype);
+  if (isarray == true){
+    for (var a = 0; a < casetype.length; a++){
+      if (casetype[a] == "camel" || casetype[a] == "pascal" || casetype[a] ==
+       "title" || casetype[a] == "snake"|| casetype[a] == "kebab" ){
+        caselist.unshift(casetype[a]);
+      }
+      else {
+        caselist.push(casetype[a]);
+      }
+    }
+  }
+  else {
+    caselist.push(casetype);
+  }
+  for (var x = 0; x < caselist.length; x++){
+    for (var i = 0; i < input.length; i++){
+
+      if (caselist[x] == "camel" || caselist[x] == "pascal" || caselist[x] ==
+       "title" || caselist[x] == "snake"|| caselist[x] == "kebab" ){
+        outputMessage = firstcase(input, caselist[x]);
+      }
+
+      if (caselist[x] == "vowel" || caselist[x] == "consonant"){
+        if (firstcase_message.length > 0){
+          outputMessage = secondcase(firstcase_message, caselist[x]);
+        }
+        else {
+          outputMessage = secondcase(input, caselist[x]);
+        }
+      }
+      else if (caselist[x] == "upper" || caselist[x] == "lower"){
+        if (firstcase_message.length > 0){
+          outputMessage = thirdcase(firstcase_message, caselist[x]);
+        }
+        else {
+          outputMessage = thirdcase(input, caselist[x]);
         }
       }
     }
   }
-  if (casetype == 'camel'){
-    outputMessage = space_replace(outputMessage, '');
-  }
-  if (casetype == 'pascal'){
-    outputMessage = space_replace(outputMessage, '');
-  }
-  if (casetype == 'snake'){  //SNAKE - convert spaces to underscore
-    outputMessage = space_replace(input, '_');
-  }
-  if (casetype == 'kebab'){  //KEBAB - convert spaces to dash
-    outputMessage = space_replace(input, '-');
-  }
-  if (casetype == 'vowel'){  //VOWEL - every vowel UpperCase
-    for (var a = 0; a < input.length; a++){
-      if (input[a] == 'a' ||input[a] == 'e' ||input[a] == 'i' ||input[a] == 'o' ||input[a] == 'u'){
-        outputMessage += input[a].toUpperCase();
-      }
-      else {
-        outputMessage += input[a];
-      }
-    }
-  }
-  if (casetype == 'consonant'){  //CONSONANT - every consonant UpperCase
-    for (var a = 0; a < input.length; a++){
-      if (input[a] != 'a' && input[a] != 'e' && input[a] != 'i' && input[a] != 'o' && input[a] != 'u'){
-        outputMessage += input[a].toUpperCase();
-      }
-      else {
-        outputMessage += input[a];
-      }
-    }
-  }
-  if (casetype == 'upper'){  //UPPER - convert to Upper
-    for (var u = 0; u < input.length; u++){
-      outputMessage += input[u].toUpperCase()
-    }
-  }
-  if (casetype == 'lower'){  //LOWER - convert to Lower
-    for (var u = 0; u < input.length; u++){
-      outputMessage += input[u].toLowerCase()
-    }
-  }
-  console.log(outputMessage)
-  return outputMessage
+
+  console.log(outputMessage);
+  return outputMessage;
 }
 
 makeCase("this is a string", "camel");
